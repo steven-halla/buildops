f33d4611e983c6acf2c2ece61520caeefdd05dcd
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-03-26 18:04:24 -0700

    "styling div container for returned objects"

--- src/views/DisplayMyData.tsx
+++ src/views/DisplayMyData.tsx
@@ -4,8 +4,21 @@ import React, {FC, useState} from "react";
 
 const DisplayMyDataDiv = styled.div`
   
-
- 
+  .data-container {
+    display: flex;
+    justify-content: center;
+    margin: auto;
+    padding: 2px;
+    max-width:55em;
+   
+    
+    
+    
+    border-style: solid;
+    color: red;
+    border-color: red;
+    background-color: skyblue;
+  }
   
   
 `;
@@ -22,17 +35,27 @@ export const DisplayMyData: FC = () => {
     {ProductName: "ergonomic keyboard", ProductDescription: "Super comfy keyboard"},
     {ProductName: "mac book pro", ProductDescription: "My favorite laptop"}
   ]);
+  // build this later one step at a time . focus on check boxes next
+  // const addProductHandler = () => {
+  //   let newProduct = {ProductName:"test",ProductDescription:"testing this"};
+  //   let ProductArray = products.concat(newProduct);
+  //     setProducts(products)
+  // }
 
   return (
-    <div>
+    <DisplayMyDataDiv>
       <p>I am displaying data</p>
-      <ul>
-        {
-          products.map(productObjects => (
-            <li>{productObjects.ProductName}   :    {productObjects.ProductDescription}</li>
-            )
-         )}
-      </ul>
-    </div>
+      <div className="data-container">
+        <ul>
+          {
+            products.map(productObjects => (
+                <li>{productObjects.ProductName}   :    {productObjects.ProductDescription}</li>
+              )
+            )}
+        </ul>
+      </div>
+
+      {/*<button onClick={addProductHandler}>Add new product</button> lets build this later*/}
+    </DisplayMyDataDiv>
   );
 }
